1644803b95cab7bf38998be2af2e642619ec59b8
szymyymy <szymon.wilczynski@interia.eu>
2018-07-03 19:28:09 +0200

    Second Commit

---
+++ src/main/java/com/kodilla/KodillaCourse2Application.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourse2Application.class, args);
+    }
+}
